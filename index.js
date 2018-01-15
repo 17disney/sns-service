7a6c16336e1bb007700b97eddbe4634a275d1169
xank <xank@qq.com>
2018-01-15 10:49:44 +0800

    production

--- index.js
+++ index.js
@@ -73,12 +73,16 @@ app.use((err, req, res, next) => {
 })
 
 // 监听端口，启动程序
-var key = fs.readFileSync('./cert/privatekey.pem', 'utf8')
-var cert = fs.readFileSync('./cert/certificate.crt', 'utf8')
-var credentials = { key, cert }
 
-var httpsServer = https.createServer(credentials, app)
-httpsServer.listen(443)
+if (process.env.NODE_ENV === 'production') {
+  http.createServer(app).listen(config.port)
+} else {
+  var key = fs.readFileSync('./cert/privatekey.pem', 'utf8')
+  var cert = fs.readFileSync('./cert/certificate.crt', 'utf8')
+  var credentials = { key, cert }
+
+  var httpsServer = https.createServer(credentials, app)
+  httpsServer.listen(443)
+}
 
 console.log('Disney-SNS')
-
